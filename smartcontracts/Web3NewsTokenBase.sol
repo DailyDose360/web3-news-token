1d47e21acedfb1dd58410cca930a2e0a7a82f52c
Martin Holy <mholy1983@gmail.com>
2023-04-08 23:02:35 +0200

    fix the openzeppelin import path

--- smartcontracts/Web3NewsTokenBase.sol
+++ smartcontracts/Web3NewsTokenBase.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.3/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3/contracts/token/ERC20/ERC20.sol";
 
 abstract contract Web3NewsTokenBase is ERC20 {
     address public admin;
